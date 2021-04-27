a95e0f587fd9e97c7ade276746aefe9fc5a54a96
Dan Wu <73746676+Developer-Wu@users.noreply.github.com>
2021-04-27 17:00:13 +0800

    Update main.py

--- rain-check/main.py
+++ rain-check/main.py
@@ -7,7 +7,7 @@ account_sid = 'ACC_SID'
 auth_token = 'AUTH_TOKEN'
 
 
-parameters = {'lat':43.222015, 'lon':76.851250, 'appid':'cc69b8a8fccc77a022c4d90468b6f944','exclude':'current,minutely,daily'}
+parameters = {'lat':43.222015, 'lon':76.851250, 'appid':'OPEN_WEATHER_APPID','exclude':'current,minutely,daily'}
 response = requests.get(url='https://api.openweathermap.org/data/2.5/onecall', params=parameters)
 response.raise_for_status()
 weather_data = response.json()
@@ -22,7 +22,7 @@ if will_rain:
         .create(
         body="It's going to rain today. Remember to bring an ☔️",
         from_='+15612207867',
-        to='+85295355691'
+        to='TO_PHONE_NUMBER'
     )
     print(message.status)
 
